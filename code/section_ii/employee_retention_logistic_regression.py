7500f53bf134d388bfe605d0d5a3d4568a5fbe86
Thomas Nield <thomasnield@live.com>
2019-06-02 15:00:49 -0500

    Update employee_retention_logistic_regression.py

--- code/section_ii/employee_retention_logistic_regression.py
+++ code/section_ii/employee_retention_logistic_regression.py
@@ -31,8 +31,9 @@ iterations = 50_000
 
 # calculate maximum likelihood
 
+# Closer to true (1.0) recommends dark font, closer to false (0.0) recommends light font
 def predict_probability(sex, age, promotions, years_employed):
-    x = round(-(b0 + (b1 * sex) + (b2 * age) + (b3 * promotions) + (b4 * years_employed)), 4)
+    x = -(b0 + (b1 * sex) + (b2 * age) + (b3 * promotions) + (b4 * years_employed))
     odds = exp(x)
     p = 1.0 / (1.0 + odds)
     return p
@@ -88,12 +89,13 @@ print("1.0 / (1 + exp(-({0} + {1}*s + {2}*a + {3}*p + {4}*y))".format(b0, b1, b2
 print("BEST LIKELIHOOD: {0}".format(math.exp(best_likelihood)))
 
 
-# Interact and test with new data
+# Interact and test with new colors
 def predict_employee_will_stay(sex, age, promotions, years_employed):
-    if predict_probability(sex, age, promotions, years_employed) >= .5:
-        return "WILL LEAVE"
+    probability_of_leaving = predict_probability(sex, age, promotions, years_employed)
+    if probability_of_leaving >= .5:
+        return "WILL LEAVE, {0}% chance of leaving".format(round(probability_of_leaving * 100.0,2))
     else:
-        return "WILL STAY"
+        return "WILL STAY, {0}% chance of leaving".format(round(probability_of_leaving * 100.0,2))
 
 
 while True:
