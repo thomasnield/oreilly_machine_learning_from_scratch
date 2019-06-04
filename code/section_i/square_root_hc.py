fd6ecb1ba0c117198d8ef64e9a15cf6e8f5759c0
Thomas Nield <thomasnield@live.com>
2019-06-04 11:09:03 -0500

    Update square_root_hc.py

--- code/section_i/square_root_hc.py
+++ code/section_i/square_root_hc.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 # Use hill climbing to estimate a square root
@@ -6,7 +7,7 @@ value = 11.0
 sqrt_candidate = value / 2.0
 
 for i in range(100000):
-    adjust = np.random.standard_t(3, 1)[0]
+    adjust = np.random.normal()
     new_candidate =  sqrt_candidate + adjust 
     
     if  (value - sqrt_candidate ** 2) ** 2 > (value - new_candidate ** 2) ** 2:
