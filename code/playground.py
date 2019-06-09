74d7773040ed6c5742221c0b5719987f93d70074
Thomas Nield <thomasnield@live.com>
2019-06-09 17:36:47 -0500

    continue decision tree implementation

--- code/playground.py
+++ code/playground.py
@@ -1,27 +1,10 @@
-import numpy as np
-
-centroids = np.array([
-    [10, 10],
-    [0,0],
-    [20,20]
-])
-
-points = np.array([
-    [1,1],
-    [2,2],
-    [7,7],
-    [8,8],
-    [17,17],
-    [19,19]
-])
-
-distances = np.sqrt(((points - centroids[:,np.newaxis])**2).sum(axis=2))
-
-min_distances_indices = np.argmin(distances,axis=0)
-min_distances = distances * np.argmin(distances, axis=0)
-new_loss = (min_distances ** 2).sum(axis=0)
 
 
-print(distances)
-print("\r\n\r\n")
-print(min_distances)
+
+feature_values = [0,2,4,6,8]
+feature_values2 = feature_values.copy()
+feature_values2.pop(0)
+
+zip_avgs = [(zipped[0] + zipped[1]) / 2 for zipped in zip(feature_values, feature_values2)]
+
+print(zip_avgs)
