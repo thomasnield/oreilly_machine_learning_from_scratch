ff34897af09d2956523d4704a978c6897679bdcf
Thomas Nield <thomasnield@live.com>
2019-06-09 09:14:26 -0500

    start decisiontree module

---
+++ code/section_v/employment_retention_decision_tree.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+class EmployeeRetention:
+    def __init__(self, sex, age, promotions, years_employed, did_quit):
+        self.sex = sex
+        self.age = age
+        self.promotions = promotions
+        self.years_employed = years_employed
+        self.did_quit = did_quit
+
+
+employee_data = [(EmployeeRetention(row[0], row[1], row[2], row[3], row[4])) for index, row in
+                 pd.read_csv("https://tinyurl.com/y6r7qjrp").iterrows()]
+
+class Feature:
+    def __init__(self, feature_name, feature_type):
+        self.feature_name = feature_name
+        self.feature_type = feature_type
+
+
+class TreeNode:
+    def __init__(self, feature):
+        self.feature = feature
