7b9c3c829b2bb531d262e233020682aea07125c1
Thomas Nield <thomasnield@live.com>
2019-06-08 19:13:02 -0500

    successful implementation of naive bayes

--- code/section_iv/naive_bayes_email_spam.py
+++ code/section_iv/naive_bayes_email_spam.py
@@ -1,4 +1,4 @@
-#TODO Still not working
+# TODO Still not working
 import re
 
 import math
@@ -16,7 +16,8 @@ emails = [
     Email("Viagra prescription for less", True),
     Email("Even better than Viagra, try this new prescription drug", True),
 
-    Email("Hey, I left my phone at home. Email me if you need anything. I'll be in a meeting for the afternoon.",False),
+    Email("Hey, I left my phone at home. Email me if you need anything. I'll be in a meeting for the afternoon.",
+          False),
     Email("Please see attachment for notes on today's meeting. Interesting findings on your market research.", False),
     Email("An item on your Amazon wish list received a discount", False),
     Email("Your prescription drug order is ready", False),
@@ -59,37 +60,43 @@ for email in emails:
 
 # Create functions to calculate probability of word occuring in spam or not spam
 # add a little .1 and .2 to numerator/denominator respectively to prevent 0 division
-def prob_word_appears_in_spam(word):
-    return (.1 + spam_count_by_word.get(word,0)) / (.2 + spam_email_count)
+def prob_word_appears_in_spam(w):
+    return (.1 + spam_count_by_word.get(w, 0)) / (.2 + spam_email_count)
 
 
-def prob_word_appears_in_non_spam(word):
-    return (.1 + non_spam_count_by_word.get(word,0)) / (.2 + non_spam_email_count)
+def prob_word_appears_in_non_spam(w):
+    return (.1 + non_spam_count_by_word.get(w, 0)) / (.2 + non_spam_email_count)
 
 
 # Here we go! Naive Bayes happens here
 def spam_score_for_message(message):
-    message_words = [break_up_words(message)]
+    message_words = break_up_words(message)
 
-    probability_of_spam = 0.0
+    total_spam_probability = 0.0
     for w in all_emails_count_by_word:
         if w in message_words:
-            probability_of_spam += math.log(prob_word_appears_in_spam(w))
+            total_spam_probability += math.log(prob_word_appears_in_spam(w))
         else:
-            probability_of_spam += math.log(1.0 - prob_word_appears_in_spam(w))
+            total_spam_probability += math.log(1.0 - prob_word_appears_in_spam(w))
 
-    probability_of_not_spam = 0.0
+    total_not_spam_probability = 0.0
     for w in all_emails_count_by_word:
         if w in message_words:
-            probability_of_not_spam += math.log(prob_word_appears_in_non_spam(w))
+            total_not_spam_probability += math.log(prob_word_appears_in_non_spam(w))
         else:
-            probability_of_not_spam += math.log(1.0 - prob_word_appears_in_non_spam(w))
+            total_not_spam_probability += math.log(1.0 - prob_word_appears_in_non_spam(w))
 
-    return math.exp(probability_of_spam) / (math.exp(probability_of_spam) + math.exp(probability_of_not_spam))
+    return math.exp(total_spam_probability) / (math.exp(total_spam_probability) + math.exp(total_not_spam_probability))
+
 
 # Test a new message
-message1 = "discount viagra wholesale, hurry while this offer lasts"
-message2 = "interesting meeting on amazon cloud services discount program"
+#message1 = "discount viagra wholesale, hurry while this offer lasts"
+#message2 = "interesting meeting on amazon cloud services discount program"
 
-print(spam_score_for_message(message1))
-print(spam_score_for_message(message2))
+#print("{0}% likely to be spam: {1}".format(spam_score_for_message(message1), message1))
+#print(spam_score_for_message(message2))
+
+while True:
+    message = input("Input an email message to calculate probability of spam:")
+    probability_of_spam = spam_score_for_message(message)
+    print("Spam probability: {0}%".format(round(probability_of_spam * 100.0, 6)))
