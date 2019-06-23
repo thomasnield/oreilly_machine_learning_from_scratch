43510176d7b3a8fdbd001feae12b56a862d74dec
Thomas Nield <thomasnield@live.com>
2019-06-23 13:24:06 -0500

    implement multiple linear regression

--- code/section_ii/linear_regression_hc.py
+++ code/section_ii/linear_regression_hc.py
@@ -10,18 +10,16 @@ class Point:
     def __str__(self):
         return "{0},{1}".format(self.x, self.y)
 
-points = [(Point(row.x, row.y)) for index, row in pd.read_csv("https://tinyurl.com/y58sesrr").iterrows()]
 
+points = [(Point(row.x, row.y)) for index, row in pd.read_csv("https://bit.ly/2KF29Bd").iterrows()]
 
 # Building the model
 m = 0.0
 b = 0.0
 
-
 epochs = 150000  # The number of iterations to perform
 
 n = float(len(points))  # Number of points
-
 
 best_loss = 10000000000000.0  # Initialize with a really large value
 
@@ -29,8 +27,8 @@ for i in range(epochs):
 
     # Randomly adjust "m" or "b"
 
-    m_adjust = np.random.standard_t(3, 1)[0] #or use normalvariate(0, 1)
-    b_adjust = np.random.standard_t(3, 1)[0] #or use normalvariate(0, 1)
+    m_adjust = np.random.normal()
+    b_adjust = np.random.normal()
 
     m += m_adjust
     b += b_adjust
@@ -38,7 +36,7 @@ for i in range(epochs):
     # Calculate loss, which is total mean squared error
     new_loss = 0.0
     for p in points:
-        new_loss += (p.y - (m*p.x + b))**2
+        new_loss += (p.y - (m * p.x + b)) ** 2
 
     # If loss has improved, keep new values. Otherwise revert.
     if new_loss < best_loss:
@@ -48,4 +46,4 @@ for i in range(epochs):
         m -= m_adjust
         b -= b_adjust
 
-print("y = {0}x + {1}".format(m,b))
+print("y = {0}x + {1}".format(m, b))
