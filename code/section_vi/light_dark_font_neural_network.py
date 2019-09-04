d013aaa1a66e481849a3ca5967b879fc9acc6a91
Thomas Nield <thomasnield@live.com>
2019-09-04 12:33:18 -0500

    wip

--- code/section_vi/light_dark_font_neural_network.py
+++ code/section_vi/light_dark_font_neural_network.py
@@ -8,7 +8,7 @@ training_colors = pd.read_csv("https://tinyurl.com/y2qmhfsr")
 # Extract the input columns
 input_colors = training_colors.iloc[:,0:3].values.transpose()
 
-# Extract output column
+# Extract output column, and generate an opposite column where 1 is 0 and 0 is 1.
 output_actuals = np.vstack((training_colors.iloc[:, -1].values.transpose(), -1 * (training_colors.iloc[:, -1].values.transpose() - 1)))
 
 # Build neural network
@@ -16,6 +16,9 @@ input_weights = np.random.rand(3, 3)
 middle_weights = np.random.rand(3, 3)
 output_weights = np.random.rand(2, 3)
 
+# Activation functions
+def tanh(x):
+    return np.tanh(x)
 
 def relu(x):
     return np.maximum(x, 0)
@@ -23,6 +26,7 @@ def relu(x):
 def softmax(x):
     return special.softmax(x, axis=0)
 
+# Execute training with hill-climbing
 for i in range(10000):
     training_output = softmax(output_weights.dot(relu(middle_weights.dot(input_weights.dot(input_colors)))))
 
