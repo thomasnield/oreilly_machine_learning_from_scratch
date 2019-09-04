1c3620acab6a2cc370e8189c3dbcbf6c1a65522e
Thomas Nield <thomasnield@live.com>
2019-09-04 12:18:03 -0500

    start implementing neural network

--- code/section_vi/light_dark_font_neural_network.py
+++ code/section_vi/light_dark_font_neural_network.py
@@ -1,12 +1,27 @@
 import numpy as np
+import pandas as pd
+from scipy import special
 
-training_colors = np.recfromcsv("https://tinyurl.com/y2qmhfsr")
+
+training_colors = pd.read_csv("https://tinyurl.com/y2qmhfsr")
+
+# Extract the input columns
+input_colors = training_colors.iloc[:,0:3].values.transpose()
+
+# Extract output column
+output_shades = training_colors.iloc[:,-1].values.transpose()
 
 # Build neural network
+input_weights = np.random.rand(3, 3)
+middle_weights = np.random.rand(3, 3)
+output_weights = np.random.rand(2, 3)
 
-input_layer = np.random.rand(3,1)
-middle_layer = np.random.rand(3,1)
-output_layer = np.random.rand(2,1)
+
+def relu(x):
+    return np.maximum(x, 0)
+
+def softmax(x):
+    return special.softmax(x, axis=0)
 
 
-print(input_layer)
+print(softmax(output_weights.dot(relu(middle_weights.dot(input_weights.dot(input_colors))))))
