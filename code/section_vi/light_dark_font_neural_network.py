85525bd25dd1adf0aa92a340db50d219401fbb23
Thomas Nield <thomasnield@live.com>
2019-09-17 09:18:24 -0500

    Update light_dark_font_neural_network.py

--- code/section_vi/light_dark_font_neural_network.py
+++ code/section_vi/light_dark_font_neural_network.py
@@ -18,11 +18,9 @@ actual_outputs = np.vstack(
     (training_data.iloc[:, -1].values.transpose(), -1 * (training_data.iloc[:, -1].values.transpose() - 1)))
 
 # Build neural network with weights and biases
-input_weights = np.random.rand(3, 3)
 middle_weights = np.random.rand(3, 3)
 output_weights = np.random.rand(2, 3)
 
-input_bias = np.random.rand(3, 1)
 middle_bias = np.random.rand(3, 1)
 output_bias = np.random.rand(2, 1)
 
@@ -31,91 +29,78 @@ output_bias = np.random.rand(2, 1)
 def tanh(x):
     return np.tanh(x)
 
+
 def relu(x):
     return np.maximum(x, 0)
 
+
 def softmax(x):
     return special.softmax(x, axis=0)
+
 
 best_loss = 10_000_000_000
 
 # Execute training with hill-climbing
 for i in range(1_000_000):
 
-    # 32 hyperparameters to randomly select from
-    random_select = np.random.randint(0, 32)
+    # 20 hyper-parameters to randomly select from
+    # Each parameter needs to be uniformly likely to be selected
+    # So we will use some random number strategies to
+    random_select = np.random.randint(0, 20)
     random_adjust = np.random.normal() * learning_rate
     random_row = 0
     random_col = 0
 
+    # Randomly adjust middle layer weight
     if random_select < 9:
         random_row = np.random.randint(0, 3)
         random_col = np.random.randint(0, 3)
 
-        if input_weights[random_row, random_col] + random_adjust < -1.0:
-            random_adjust = -1.0 - input_weights[random_row, random_col]
-        if input_weights[random_row, random_col] + random_adjust > 1.0:
-            random_adjust = 1.0 - input_weights[random_row, random_col]
-
-        input_weights[random_row, random_col] += random_adjust
-
-    elif random_select < 18:
-        random_row = np.random.randint(0, 3)
-        random_col = np.random.randint(0, 3)
-
-        if input_weights[random_row, random_col] + random_adjust < -1.0:
+        if middle_weights[random_row, random_col] + random_adjust < -1.0:
             random_adjust = -1.0 - middle_weights[random_row, random_col]
-        if input_weights[random_row, random_col] + random_adjust > 1.0:
+        if middle_weights[random_row, random_col] + random_adjust > 1.0:
             random_adjust = 1.0 - middle_weights[random_row, random_col]
 
         middle_weights[random_row, random_col] += random_adjust
 
-    elif random_select < 24:
-        random_row = np.random.randint(2)
-        random_col = np.random.randint(3)
+    # Randomly adjust outer layer weight
+    elif random_select < 15:
+        random_row = np.random.randint(0, 2)
+        random_col = np.random.randint(0, 3)
 
-        if input_weights[random_row, random_col] + random_adjust < -1.0:
+        if output_weights[random_row, random_col] + random_adjust < -1.0:
             random_adjust = -1.0 - output_weights[random_row, random_col]
-        if input_weights[random_row, random_col] + random_adjust > 1.0:
+        if output_weights[random_row, random_col] + random_adjust > 1.0:
             random_adjust = 1.0 - output_weights[random_row, random_col]
 
         output_weights[random_row, random_col] += random_adjust
 
-    elif random_select < 27:
+    # Randomly adjust middle layer bias
+    elif random_select < 18:
         random_row = np.random.randint(3)
         random_col = 0
 
-        if input_weights[random_row, random_col] + random_adjust < 0.0:
-            random_adjust = 0.0 - input_bias[random_row, random_col]
-        if input_weights[random_row, random_col] + random_adjust > 1.0:
-            random_adjust = 1.0 - input_bias[random_row, random_col]
-
-        input_bias[random_row, random_col] += random_adjust
-
-    elif random_select < 30:
-        random_row = np.random.randint(3)
-        random_col = 0
-
-        if input_weights[random_row, random_col] + random_adjust < 0.0:
+        if middle_bias[random_row, random_col] + random_adjust < 0.0:
             random_adjust = 0.0 - middle_bias[random_row, random_col]
-        if input_weights[random_row, random_col] + random_adjust > 1.0:
+        if middle_bias[random_row, random_col] + random_adjust > 1.0:
             random_adjust = 1.0 - middle_bias[random_row, random_col]
 
         middle_bias[random_row, random_col] += random_adjust
 
-    elif random_select < 32:
+    # Randomly adjust outer layer bias
+    elif random_select < 20:
         random_row = np.random.randint(2)
         random_col = 0
 
-        if input_weights[random_row, random_col] + random_adjust < 0.0:
+        if output_bias[random_row, random_col] + random_adjust < 0.0:
             random_adjust = 0.0 - output_bias[random_row, random_col]
-        if input_weights[random_row, random_col] + random_adjust > 1.0:
+        if output_bias[random_row, random_col] + random_adjust > 1.0:
             random_adjust = 1.0 - output_bias[random_row, random_col]
 
         output_bias[random_row, random_col] += random_adjust
 
     # Calculate outputs with the given weights, biases, and activation functions for all three layers
-    training_outputs = softmax(output_bias + output_weights.dot(tanh(middle_bias + middle_weights.dot(relu(input_bias + input_weights.dot(training_inputs))))))
+    training_outputs = softmax(output_bias + output_weights.dot(relu(middle_bias + middle_weights.dot(training_inputs))))
 
     # Calculate the mean squared loss
     mean_loss = np.sum((actual_outputs - training_outputs) ** 2) / training_data_count
@@ -125,37 +110,34 @@ for i in range(1_000_000):
         best_loss = mean_loss
         print(best_loss)
 
+    # Undo the random adjust if loss hasn't improved
     elif random_select < 9:
-        input_weights[random_row, random_col] -= random_adjust
-
-    elif random_select < 18:
         middle_weights[random_row, random_col] -= random_adjust
 
-    elif random_select < 24:
+    elif random_select < 15:
         output_weights[random_row, random_col] -= random_adjust
 
-    elif random_select < 27:
-        input_bias[random_row, random_col] -= random_adjust
-
-    elif random_select < 30:
+    elif random_select < 18:
         middle_bias[random_row, random_col] -= random_adjust
 
-    elif random_select < 32:
-        output_bias[random_row, random_col] -= random_adjust
+    elif random_select < 20:
+        middle_weights[random_row, random_col] -= random_adjust
 
 
 # Interact and test with new colors
 def predict_probability(r, g, b):
-    input_colors = np.array([r,g,b]).transpose() / 255
-    output = softmax(output_bias + output_weights.dot(tanh(middle_bias + middle_weights.dot(relu(input_bias + input_weights.dot(input_colors))))))
+    input_colors = np.array([r, g, b]).transpose() / 255
+    output = softmax(output_bias + output_weights.dot(relu(middle_bias + middle_weights.dot(input_colors))))
     return output
+
 
 def predict_font_shade(r, g, b):
     output_values = predict_probability(r, g, b)
-    if output_values[0,0] > output_values[1,0]:
+    if output_values[0, 0] > output_values[1, 0]:
         return "DARK"
     else:
         return "LIGHT"
+
 
 while True:
     n = input("Predict light or dark font. Input values R,G,B: ")
