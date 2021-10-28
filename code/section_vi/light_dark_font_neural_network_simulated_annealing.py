485d0584939149f16e0825ff17d98a61c49582fd
Thomas Nield <thomasnield@live.com>
2021-10-28 10:30:17 -0500

    Update light_dark_font_neural_network_simulated_annealing.py

--- code/section_vi/light_dark_font_neural_network_simulated_annealing.py
+++ code/section_vi/light_dark_font_neural_network_simulated_annealing.py
@@ -8,14 +8,17 @@ training_data_count = len(training_data.index)
 # Learning rate controls how slowly we approach a solution
 # Make it too small, it will take too long to run.
 # Make it too big, it will likely overshoot and miss the solution.
-learning_rate = 0.1
+learning_rate = 0.01
+sample_size = 10
 
 # Extract the input columns, scale down by 255
 training_inputs = (training_data.iloc[:, 0:3].values.transpose() / 255.0 * .99) + .01
 
 # Extract output column, and generate an opposite column where 1 is 0 and 0 is 1.
-actual_outputs = np.vstack(
+training_outputs = np.vstack(
     (training_data.iloc[:, -1].values.transpose(), -1 * (training_data.iloc[:, -1].values.transpose() - 1)))
+
+n = len(training_data.index)
 
 # Build neural network with weights and biases
 middle_weights = np.random.rand(3, 3)
@@ -29,17 +32,10 @@ best_output_weights = None
 best_middle_bias = None
 best_output_bias = None
 
-
 # Activation functions
+softplus = lambda x: np.log(1 + np.exp(x))
+logistic = lambda x: 1 / (1 + np.exp(-x))
 
-def relu(x):
-    return np.maximum(x, 0)
-
-def softmax(x):
-    return special.softmax(x, axis=0)
-
-def tanh(x):
-    return np.tanh(x)
 
 best_loss = 10_000_000_000
 
@@ -47,7 +43,7 @@ best_loss = 10_000_000_000
 # This is similar to hill-climbing except it occasionally allows inferior moves hoping to find a better solution
 # This is covered in depth in another online training "Intro to Mathematical Optimization"
 
-epochs = 1_000_000
+epochs = 10_000_000
 temperature = 120.0
 decrement = temperature / epochs
 new_loss = 1_000_000_000.0
@@ -113,11 +109,16 @@ for i in range(epochs):
 
         output_bias[random_row, random_col] += random_adjust
 
+
+    #idx = np.random.choice(n, sample_size, replace=False)
+    #input_sample = training_inputs[:, idx]
+    #output_sample = training_outputs[:, idx]
+
     # Calculate outputs with the given weights, biases, and activation functions for all three layers
-    training_outputs = softmax(output_bias + output_weights.dot(tanh(middle_bias + middle_weights.dot(training_inputs))))
+    predicted_outputs = logistic(output_bias + output_weights.dot(softplus(middle_bias + middle_weights.dot(training_inputs))))
 
     # Calculate the mean squared loss
-    new_loss = np.sum((actual_outputs - training_outputs) ** 2) / training_data_count
+    new_loss = np.sum((training_outputs - predicted_outputs) ** 2) / training_data_count
 
     # If the loss improves, keep the random adjustment. Otherwise revert.
     if new_loss < best_loss:
@@ -162,7 +163,7 @@ output_bias = best_output_bias
 # Interact and test with new colors
 def predict_probability(r, g, b):
     input_colors = np.array([r, g, b]).transpose() / 255
-    output = softmax(output_bias + output_weights.dot(tanh(middle_bias + middle_weights.dot(input_colors))))
+    output = logistic(output_bias + output_weights.dot(softplus(middle_bias + middle_weights.dot(input_colors))))
     return output
 
 
@@ -175,6 +176,9 @@ def predict_font_shade(r, g, b):
 
 
 while True:
-    n = input("Predict light or dark font. Input values R,G,B: ")
-    (r, g, b) = n.split(",")
-    print(predict_font_shade(int(r), int(g), int(b)))
+    try:
+        n = input("Predict light or dark font. Input values R,G,B: ")
+        (r, g, b) = n.split(",")
+        print(predict_font_shade(int(r), int(g), int(b)))
+    except Exception as e:
+        print(e)
